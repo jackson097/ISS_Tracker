8d0eefa4438061f63a800e97bfc70e966d814ea7
Jackson Newlands <jacksonn@stratinfotech.com>
2019-11-13 17:11:15 -0500

    Added get_longitude and get_latitude functions

--- api.py
+++ api.py
@@ -2,23 +2,25 @@ import requests
 import json
 
 """
+Returns current ISS information in JSON format relative to your position on Earth
+Parameters: latitude  - your current latitude
+		 longitude - your current longitude
 """
-def get_iss_information(latitude, longitude):
-	return requests.get("http://api.open-notify.org/iss-pass.json?lat=latitude&lon=longitude").json()
-
-
+def get_iss_pass_times(latitude, longitude):
+	return requests.get("http://api.open-notify.org/iss-pass.json?lat=" + latitude + "&lon=" + longitude).json()
+	
 """
-def jprint(obj):
-    # Create a formatted string of the JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-latitude_and_longitude = {
-	"lat": 40.71,
-	"lon": -74
-}
-
-response = requests.get("http://api.open-notify.org/iss-pass.json", params=latitude_and_longitude)
-
-jprint(response.json())
+Returns the current coordinates of the ISS in JSON form
 """
+def get_iss_coordinates():
+	return requests.get("http://api.open-notify.org/iss-now.json").json()
+	
+"""
+"""
+def get_longitude():
+	return get_iss_coordinates()['iss_position']['longitude']
+	
+"""
+"""
+def get_latitude():
+	return get_iss_coordinates()['iss_position']['latitude']

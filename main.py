a0699297c58cec3d48b2754ca5aea6f90398345a
Jackson Newlands <jacksonn@stratinfotech.com>
2019-11-14 09:30:07 -0500

    Organized the code and added external functions for better readability

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from api import get_iss_pass_times, get_longitude, get_latitude
 from input_validation import is_valid_latitude, is_valid_longitude
+from print_results import print_ISS_pass_info
 
-# Accept latitude input and validate
-latitude = raw_input("\nEnter your current latitude: ")
-while(is_valid_latitude(latitude) == False):
-	latitude = raw_input("\nInvalid latitude. Try again: ")
-
-# Accept longitude input and validate
-longitude = raw_input("Enter your current longitude: ")
-while(is_valid_longitude(longitude) == False):
-	longitude = raw_input("\nInvalid longitude. Try again: ")
-
-iss_pass_times = get_iss_pass_times(latitude, longitude)
+print("Enter a longitude and latitude to see ISS pass information over those coordinates.")
 
 # Retrieve and store current latitude and longitude values of the ISS
 ISS_longitude = get_longitude()
 ISS_latitude = get_latitude()
 
-print
-print(iss_pass_times)
+# Print the current coordinates of the ISS
 print("\nCurrent ISS coordinates: {}, {}".format(ISS_latitude, ISS_longitude))
+
+# Accept latitude input and validate
+latitude = raw_input("\nEnter latitude: ")
+while(is_valid_latitude(latitude) == False):
+	latitude = raw_input("\nInvalid latitude. Try again: ")
+
+# Accept longitude input and validate
+longitude = raw_input("Enter longitude: ")
+while(is_valid_longitude(longitude) == False):
+	longitude = raw_input("\nInvalid longitude. Try again: ")
+
+# Print the ISS pass times over the location provided
+print("\nISS passes over (" + latitude + "," + longitude + "):")
+iss_pass_times = get_iss_pass_times(latitude, longitude)
+for index in range(0, len(iss_pass_times)-1):
+	print_ISS_pass_info(index, iss_pass_times)

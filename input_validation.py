8ccb1ee79d0a6598509c3578f2b0e6e7d2eb7173
Jackson Newlands <jacksonn@stratinfotech.com>
2019-11-14 09:30:50 -0500

    Improved input validation function

--- input_validation.py
+++ input_validation.py
@@ -1,13 +1,10 @@
-### Latitude must be between -90.0 and 90.0
-### Latitude must be between -180.0 and 180.0
-
-
 """
 Check if the coordinate provided is a valid coordinate for the API call
-Parameters: type       - latitude or longitude
+Parameters: type - latitude or longitude
 			coordinate - coordinate value passed as a string
+Returns: boolean - True if the coordinate is valid, Else False
 """
-def is_valid_coordinate(type, coordinate):
+def _is_valid_coordinate(type, coordinate):
 	# Determine what the max/min coordinates are by the type
 	if type == "longitude":
 		value = 180
@@ -32,13 +29,15 @@ def is_valid_coordinate(type, coordinate):
 """
 Checks if coordinate is a valid longitude value
 Parameters: coordinate - The longitude coordinate to be validated (string)
+Returns: True if longitude is valid, Else False
 """
 def is_valid_longitude(coordinate):
-	return is_valid_coordinate("longitude", coordinate)
+	return _is_valid_coordinate("longitude", coordinate)
 
 """
 Checks if coordinate is a valid latitude value
 Parameters: coordinate - The latitude coordinate to be validated (string)
+Returns: True if latitude is valid, Else False
 """
 def is_valid_latitude(coordinate):
-	return is_valid_coordinate("latitude", coordinate)
+	return _is_valid_coordinate("latitude", coordinate)
